e3ffa62abcfb8b18990b8a69717e44bb869beea6
Logan Roberts <loganroberts@me.com>
2018-08-28 09:27:13 -0400

    Initial Commit

---
+++ TrumpTweets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrumpTweets
+//
+//  Created by Logan Roberts on 8/28/18.
+//  Copyright © 2018 Logan Roberts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
